9b657b129affc22597470b8d8c0561ccc3eee668
amel-FETTAH <amelfettah43@gmail.com>
2023-12-23 21:20:39 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import PopoutArticle from "./Componant/PopoutArticle";
+
+
 
 
 function App() {
   return (
-    <div className="App">
-      <p> hello world</p>
+    <div className="app">
+   <PopoutArticle/>  
+     
     </div>
   );
 }
 
 export default App;
+//h-full w-full 8E4E] bg-[#E7E4D5] text-center items-center pt-[50px] lg:pt-[45px] xl:pt-[20px]
